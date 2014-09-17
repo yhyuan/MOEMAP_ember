5c5e68816bc06343921fb25330df777f183edec0
Jerry Yuan <yhyuan@gmail.com>
2014-09-16 22:28:35 -0400

    remove language configure for sport fish

--- app/scripts/Configures/SportFish/configure.js
+++ app/scripts/Configures/SportFish/configure.js
@@ -2,21 +2,125 @@
 'use strict';
 var GoogleMapsAdapter = require('../scripts/GoogleMapsAdapter');
 var Util = require('../scripts/Util');
+/*English Begins*/
+var langSetting = require('../scripts/Languages/English');
+/*English Ends*/
+/*French Begins*/
+var langSetting = require('../scripts/Languages/French');
+/*French Ends*/
 window.GoogleMapsAdapter = GoogleMapsAdapter;
-var configure = {
+GoogleMapsAdapter.init({
+	langs: langSetting,
+	/*English Begins*/
+	language: "EN",
+	/*English Ends*/
+	/*French Begins*/
+	language: "FR",
+	/*French Ends*/
 	mapServices: [{
 		url: "http://www.appliomaps.lrc.gov.on.ca/ArcGIS/rest/services/MOE/sportfish/MapServer",
 		visibleLayers: [0, 1, 2]
 	}],
+	/*English Begins*/
+	otherInfoHTML: "<h2>Find a map error?</h2> \
+		<p>It is possible you may encounter inaccuracies with map locations.</p> \
+		<p>If you find an error in the location of a lake, river or stream, please contact us.  Use the <a href='mailto:sportfish.moe@ontario.ca?subject=Sport Fish Map Error'>Report an error</a> link within the map pop-up.</p> \
+		<h2>Comments</h2> \
+		<p>For comments and suggestions, email us at <a href='mailto:sportfish.moe@ontario.ca?subject=Sport Fish Map Feedback'>sportfish.moe@ontario.ca</a>.</p>",
+	/*English Ends*/
+	/*French Begins*/
+	otherInfoHTML: '<h2>Une erreur sur la carte?</h2> \
+		<p>Il est possible que des impr&eacute;cisions se soient gliss&eacute;es sur les emplacements.</p> \
+		<p>Si vous trouvez une erreur d&rsquo;emplacement d&rsquo;un lac, d&rsquo;une rivi&egrave;re ou d&rsquo;un cours d&rsquo;eau, veuillez nous en avertir. Vous pouvez utiliser le lien &laquo; <a href="mailto:sportfish.moe@ontario.ca?subject=Sport%20Fish%20Map%20Error">Signaler une erreur</a> &raquo; du menu contextuel de la carte.</p> \
+		<h2>Commentaires</h2> \
+		<p>Veuillez formuler vos commentaires ou vos suggestions par courriel &agrave; <a href="mailto:sportfish.moe@ontario.ca">sportfish.moe@ontario.ca</a>.</p>',
+	/*French Ends*/
+	/*English Begins*/
+	report_URL: "fish-consumption-report",
+	/*English Ends*/
+	/*French Begins*/
+	report_URL: "rapport-de-consommation-de-poisson",
+	/*French Ends*/	
+	/*English Begins*/
+	searchControlHTML: '<div id="searchTheMap"></div><div id="searchHelp"></div><br>\
+		<label class="element-invisible" for="map_query">Search the map</label>\
+		<input id="map_query" type="text" title="Search term" maxlength="100" size="50" onkeypress="return GoogleMapsAdapter.entsub(event)"></input>\
+		<label class="element-invisible" for="search_submit">Search</label>\
+		<input id="search_submit" type="submit" title="Search" onclick="GoogleMapsAdapter.search()" value="Search"></input>\
+		<fieldset>\
+			<input type="radio" id="searchMapLocation" name="searchGroup" checked="checked" title="Search Map Location" name="location" value="location" onclick="GoogleMapsAdapter.searchChange(this)"></input>\
+			<span class="tooltip" title="Search Map Location: Enter the name of an Ontario lake/river, city/town/township or street address to find fish consumption advice">\
+			<label class="option" for="searchMapLocation">Search Map Location</label>\
+			</span>\
+			<br/>\
+			<input type="radio" id="searchFishSpecies" name="searchGroup" title="Search Fish Species" name="species" value="species" onclick="GoogleMapsAdapter.searchChange(this)"></input>\
+			<span class="tooltip" title="Search Fish Species: Enter the name of a fish species to find lakes with fish consumption advice for the species">\
+			<label class="option" for="searchFishSpecies">Search Fish Species</label>\
+			</span>\
+			<br/>\
+			<input id="currentMapExtent" type="checkbox" name="currentExtent" title="Current Map Display" /> <label for="currentExtent" class=\'option\'>Search current map display only</label>\
+		</fieldset>\
+		<div id="information"></div>',
+	/*English Ends*/
+	/*French Begins*/
+	searchControlHTML: '<div id="searchTheMap"></div><div id="searchHelp"></div><br>\
+		<label class="element-invisible" for="map_query">Recherche carte interactive</label>\
+		<input id="map_query" type="text" title="Terme de recherche" maxlength="100" size="50" onkeypress="return GoogleMapsAdapter.entsub(event)"></input>\
+		<label class="element-invisible" for="search_submit">Recherche</label>\
+		<input id="search_submit" type="submit" title="Recherche" onclick="GoogleMapsAdapter.search()" value="Recherche"></input>\
+		<fieldset>\
+			<input type="radio" id="searchMapLocation" name="searchGroup" checked="checked" title="Recherche d\'emplacements" name="location" value="location" onclick="GoogleMapsAdapter.searchChange(this)"></input>\
+			<span class="tooltip" title="Recherche d\'emplacements : Indiquer le lieu en Ontario (lac/rivi\u00e8re, ville/canton, adresse) pour avoir des conseils sur la consommation des poissons du lieu.">\
+			<label class="option" for="searchMapLocation">Recherche d\'emplacements</label>\
+			</span>\
+			<br/>\
+			<input type="radio" id="searchFishSpecies" name="searchGroup" title="Recherche d\'esp\u00e8ces" name="species" value="species" onclick="GoogleMapsAdapter.searchChange(this)"></input>\
+			<span class="tooltip" title="Recherche d\'esp\u00e8ces : Indiquer une esp\u00e8ce de poisson pour trouver des lacs sur lesquels existent des conseils sur la consommation de l\'esp\u00e8ce.">\
+			<label class="option" for="searchFishSpecies">Recherche d\'esp\u00e8ces</label>\
+			</span>\
+			<br/>\
+			<input id="currentMapExtent" type="checkbox" name="currentExtent" title="Étendue de la carte courante" /> <label for="currentExtent" class=\'option\'>\u00c9tendue de la carte courante</label>\
+		</fieldset>\
+		<div id="information"></div>',
+	/*French Ends*/
+	identifySettings: {
+		/* radius: 1, // 1 meter. If the target layer is a polygon layer, it is useful to set the radius as a small value. If the target layer is a point layer, it is useful to increase the radius according to zoom level. */
+		identifyLayersList: [{
+			mapService: 'http://www.appliomaps.lrc.gov.on.ca/ArcGIS/rest/services/MOE/sportfish/MapServer',
+			layerID: 0,
+			/*English Begins*/
+			outFields: ['WATERBODYC', 'LOCNAME_EN', 'GUIDELOC_EN', 'LATITUDE', 'LONGITUDE']
+			/*English Ends*/
+			/*French Begins*/
+			outFields: ['WATERBODYC', 'LOCNAME_FR', 'GUIDELOC_FR', 'LATITUDE', 'LONGITUDE']
+			/*French Ends*/
+		}],
+		/*English Begins*/		
+		identifyTemplate: '<strong><%= attrs.LOCNAME_EN %></strong><br><%= Util.addBRtoLongText(attrs.GUIDELOC_EN) %><br><br>\
+			<a target=\'_blank\' href=\'fish-consumption-report?id=<%= attrs.WATERBODYC %>\'>Consumption Advisory Table</a><br><br>\
+			Latitude <b><%= Util.deciToDegree(attrs.LATITUDE, "EN") %></b> Longitude <b><%= Util.deciToDegree(attrs.LONGITUDE, "EN") %></b><br>\
+			<a href=\'mailto:sportfish.moe@ontario.ca?subject=Portal Error (Submission <%= attrs.LOCNAME_EN %>)\'>Report an error for this location</a>.<br><br>'
+		/*English Ends*/
+		/*French Begins*/
+		identifyTemplate: '<strong><%= attrs.LOCNAME_FR %></strong><br><%= Util.addBRtoLongText(attrs.GUIDELOC_FR) %><br><br>\
+			<a target=\'_blank\' href=\'rapport-de-consommation-de-poisson?id=<%= attrs.WATERBODYC %>\'>Tableau des mises en garde en mati\u00e8re de<br> consommation</a><br><br>\
+			Latitude <b><%= Util.deciToDegree(attrs.LATITUDE, "FR") %></b> Longitude <b><%= Util.deciToDegree(attrs.LONGITUDE, "FR") %></b><br>\
+			<a href=\'mailto:sportfish.moe@ontario.ca?subject=Erreur de portail (Submission <%= attrs.LOCNAME_FR %>)\'>Signalez un probl\u00e8me pour ce lieu</a>.<br><br>'
+		/*French Ends*/
+	},
 	infoWindowHeight: '140px',
 	getSearchParams: function(searchString){
-		var lang = this.language;
 		var getLakeNameSearchCondition = function(searchString) {
 			var coorsArray = searchString.split(/\s+/);
 			var str = coorsArray.join(" ").toUpperCase();
 			str = Util.replaceChar(str, "'", "''");
 			str = Util.replaceChar(str, "\u2019", "''");
-			return "UPPER(LOCNAME_" + lang + ") LIKE '%" + str + "%'";
+			/*English Begins*/
+			return "UPPER(LOCNAME_EN) LIKE '%" + str + "%'";
+			/*English Ends*/
+			/*French Begins*/
+			return "UPPER(LOCNAME_FR) LIKE '%" + str + "%'";
+			/*French Ends*/
 		};
 		var getQueryCondition = function(name){
 			var str = name.toUpperCase();
@@ -55,7 +159,12 @@ var configure = {
 				var fish = Util.wordCapitalize(Util.replaceChar(fishname, '_', ' '));
 				if (typeof(alias) === "undefined"){
 					var result = {
-						condition: "(SPECIES_" + lang + " like '%" + fishname +"%')",
+						/*English Begins*/
+						condition: "(SPECIES_EN like '%" + fishname +"%')",
+						/*English Ends*/
+						/*French Begins*/
+						condition: "(SPECIES_FR like '%" + fishname +"%')",
+						/*French Ends*/
 						information: fish
 					};
 					return result;
@@ -63,7 +172,12 @@ var configure = {
 					var res = [];
 					var fishArray = [];
 					for (var i = 0; i < alias.length; i++){
-						res.push("(SPECIES_" + lang + " like '%" + alias[i] +"%')");
+						/*English Begins*/
+						res.push("(SPECIES_EN like '%" + alias[i] +"%')");
+						/*English Ends*/
+						/*French Begins*/
+						res.push("(SPECIES_FR like '%" + alias[i] +"%')");
+						/*French Ends*/
 						var str = Util.wordCapitalize(Util.replaceChar(alias[i], '_', ' '));
 						fishArray.push(str.trim());
 					}
@@ -95,7 +209,12 @@ var configure = {
 			layerID: 0,
 			returnGeometry: true,
 			where: ($('#searchMapLocation')[0].checked) ? getLakeNameSearchCondition(searchString) : getQueryCondition(searchString).condition,
-			outFields: ['WATERBODYC', 'LOCNAME_' + lang, 'GUIDELOC_' + lang, 'LATITUDE', 'LONGITUDE']
+			/*English Begins*/
+			outFields: ['WATERBODYC', 'LOCNAME_EN', 'GUIDELOC_EN', 'LATITUDE', 'LONGITUDE']
+			/*English Ends*/
+			/*French Begins*/
+			outFields: ['WATERBODYC', 'LOCNAME_FR', 'GUIDELOC_FR', 'LATITUDE', 'LONGITUDE']
+			/*French Ends*/
 		}];
 		var options = {
 			searchString: searchString,
@@ -112,6 +231,24 @@ var configure = {
 			options: options
 		}
 	},
+	/*English Begins*/	
+	tableTemplate: '<table id="myTable" class="tablesorter" width="700" border="0" cellpadding="0" cellspacing="1">\
+		<thead><tr><th><center>Waterbody</center></th><th><center>Location</center></th><th><center>Latitude</center></th><th><center>Longitude</center></th><th><center>Consumption Advisory Table</center></th></tr></thead><tbody>\
+		<% _.each(features, function(feature) {\
+			var attrs = feature.attributes; %> \
+			<tr><td><%= attrs.LOCNAME_EN %></td><td><%= Util.addBRtoLongText(attrs.GUIDELOC_EN) %></td><td><%= Util.deciToDegree(attrs.LATITUDE, "EN") %></td><td><%= Util.deciToDegree(attrs.LONGITUDE, "EN") %></td><td><a target=\'_blank\' href=\'fish-consumption-report?id=<%= attrs.WATERBODYC  %>\'>Consumption Advisory Table</a></td></tr>\
+		<% }); %>\
+		</tbody></table>',
+		/*English Ends*/
+		/*French Begins*/
+	tableTemplate: '<table id="myTable" class="tablesorter" width="700" border="0" cellpadding="0" cellspacing="1">\
+		<thead><tr><th><center>Plan d\'eau</center></th><th><center>Lieu</center></th><th><center>Latitude</center></th><th><center>Longitude</center></th><th><center>Tableau des mises en garde en mati\u00e8re de consommation</center></th></tr></thead><tbody>\
+		<% _.each(features, function(feature) {\
+			var attrs = feature.attributes; %> \
+			<tr><td><%= attrs.LOCNAME_FR %></td><td><%= Util.addBRtoLongText(attrs.GUIDELOC_FR) %></td><td><%= Util.deciToDegree(attrs.LATITUDE, "FR") %></td><td><%= Util.deciToDegree(attrs.LONGITUDE, "FR") %></td><td><a target=\'_blank\' href=\'rapport-de-consommation-de-poisson?id=<%= attrs.WATERBODYC  %>\'>Tableau des mises en garde en mati\u00e8re de consommation</a></td></tr>\
+		<% }); %>\
+		</tbody></table>',		
+		/*French Ends*/
 	searchChange: function (type) {
 		$('#' + this.searchInputBoxDivId)[0].value = '';
 		if(type === "Substance"){
@@ -133,6 +270,4 @@ var configure = {
 				disabled: true });
 		}
 	}
-};
-configure = _.defaults(configure, configureLanguage);
-GoogleMapsAdapter.init(configure);
+});
