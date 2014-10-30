e91890a2e647b0da64e9cd8a5799c4376700c836
Jerry Yuan <yhyuan@gmail.com>
2014-10-30 15:37:20 -0400

    Facilities

--- app/scripts/concatenate.js
+++ app/scripts/concatenate.js
@@ -7,6 +7,7 @@ if (process.argv.length < 4) {
 var fs = require('fs')
 	, foldername = process.argv[2]
 	, language = process.argv[3];
+
 var config = fs.readFileSync("Configures/" + foldername + "/configure.js", 'utf8');
 var libs = config.split("JAVASCRIPT")[1].trim().split('\r\n');
 var result = '';
@@ -14,27 +15,16 @@ for (var i = 0; i < libs.length; i++) {
 	result = result + fs.readFileSync("../" + libs[i], 'utf8');
 }
 result = result + fs.readFileSync("../configures/configure.js", 'utf8');
-//console.log(result);
-
-fs.readFile("../configures/configure.js", 'utf8', function(err, data) {
-	if (err) throw err;
-	fs.readFile("../bower_components/yepnope/yepnope.1.5.4-min.js", 'utf8', function(err1, yepnope) {
-		if (err1) throw err1;
-		fs.readFile("../bower_components/underscore/underscore.js", 'utf8', function(err2, underscore) {
-			if (err2) throw err2;
-			fs.readFile("../bower_components/arcgislink/arcgislink.js", 'utf8', function(err4, arcgislink) {
-				if (err4) throw err4;
-				fs.writeFile("../configures/" + foldername + "." + language + ".js", "/*" + (new Date()) + "*/\n" + yepnope + underscore + arcgislink + data, function(err3) {
-					if(err3) {
-						console.log(err3);
-					}
-				});
-			});	
-		});		
-	});
-});
 fs.writeFileSync("../configures/" + foldername + "." + language + ".js", result);
 fs.writeFileSync("../configures/configure.js", result);
+
+libs = config.split("CSS")[1].trim().split('\r\n');
+result = '';
+for (var i = 0; i < libs.length; i++) {
+	result = result + fs.readFileSync("../" + libs[i], 'utf8');
+}
+fs.writeFileSync("../configures/" + foldername + "." + language + ".css", result);
+fs.writeFileSync("../configures/configure.css", result);
 
 fs.readFile("../configures/index.html", 'utf8', function(err, data) {
 	var array = data.split('MOEMAPJAVASCRIPT');
